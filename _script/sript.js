f100382aa70216a18a9dd5719d64219fda84da8f
Roberto Jr <contatorj.roberto@gmail.com>
2023-09-05 15:54:15 -0300

    Envio website v1.0

---
+++ _script/sript.js
@@ -0,0 +1,9 @@
+//Navbar Change Color on Scroll
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 700){
+        $("nav").addClass("dark-nav");
+    }else{
+        $("nav").removeClass("dark-nav");
+    }
+});
